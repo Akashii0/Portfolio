d86343da8fb1288f0c0aaedfcdab4f08751f8d9c
Akashii0 <abdulkid151@gmail.com>
2024-10-04 20:34:36 +0100

    updated main.py :))

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI()
+
+@app.get('/')
+def homepage(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "name": "Welcome to my Portfolio!"})
 
