b74b676f6ca97bb89c12f799cd15607d91a49ad5
Akashii0 <abdulkid151@gmail.com>
2024-10-05 16:49:27 +0100

    updated main.py :))

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-
+from fastapi.staticfiles import StaticFiles
 
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static', html=True), name="static")
 
 @app.get('/')
 def homepage(request: Request):
